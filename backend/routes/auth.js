da5d0e0f1caa06822ba1b07e00055b58fb0f59e4
Subham Sharma <subham.sharma221296@gmail.com>
2023-11-18 14:57:06 -0400

    authentication endpoints complete

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const User = require('../models/Users');
+const jwt = require("jsonwebtoken");
+const User = require("../models/Users");
 const { body, validationResult } = require("express-validator");
+const fetchuser = require('../middleware/fetchUser')
 
-// Create a user
-router.post("/createuser",
+const JWT_SECRET = "subhamsharm@";
+
+// ROUTE 1 Create a user : Signup
+router.post(
+  "/createuser",
   [
     body("email", "Enter a valid email").isEmail(),
     body("name", "Enter a valid name").isLength({ min: 3 }),
-    body("password", "Password must be atleast 6 characters").isLength({ min: 6 }),
+    body("password", "Password must be atleast 6 characters").isLength({
+      min: 6,
+    }),
   ],
   async (req, res) => {
     //If there are errors, return bad request and the errors
     const error = validationResult(req);
-  if (!error.isEmpty()) {
-    return res.status(400).json({errors: error.array()});
-  }
-  //check whether the email id already exists.
-  try{
-      let user = await User.findOne({email: req.body.email});
-      if(user){
-          return res.status(400).json({error: "User with email id already exists"})
+    if (!error.isEmpty()) {
+      return res.status(400).json({ errors: error.array() });
+    }
+    //check whether the email id already exists.
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "User with email id already exists" });
       }
 
       const salt = await bcrypt.genSalt(10);
@@ -30,14 +39,72 @@ router.post("/createuser",
         name: req.body.name,
         email: req.body.email,
         password: securePass,
-      })
-      res.json(user)
-  }
-   catch(error) {
-    console.error(error.message);
-    res.status(500).send("some error occured, try again")
-  }
+      });
+
+      const data = {
+        id: user.id,
+      };
+
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error, try again later");
+    }
   }
 );
 
-module.exports = router
+// ROUTE 2 Authenticate a returning user : Login
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    //If there are errors, return bad request and the errors
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({ errors: error.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please login with correct credentials" });
+      }
+      const pwdCompare = await bcrypt.compare(password, user.password);
+      if (!pwdCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please login with correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error, try again later");
+    }
+  }
+);
+
+// ROUTE 3 Get logged in user details
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    userId =req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user)
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error, try again later");
+  }
+});
+
+module.exports = router;
